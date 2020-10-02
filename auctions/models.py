83e56ebde08450f89878e62c54ba1d802df7dbd2
Haikal Yusuf <itshaikalhere@hotmail.sg>
2020-10-02 17:17:42 +0800

    Updated auctions models

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
+    name = models.CharField(max_length=32, default="User")
+    country = models.CharField(max_length=64, default="USA")
+
+class Listing(models.Model):
     pass
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+
